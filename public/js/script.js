9bc3bd5c6731aba5e8fb9ba578abc00d1fba0e98
Vicky Chijwani <vickychijwani@gmail.com>
2011-05-31 20:24:25 +0530

    added users list to the 'Add User' page, refactored common view portions to views/layout.haml

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,6 @@
 $(function() {
    $("form#load-tweets-form").submit(function(e) {
-      $("#message-drawer span").html("Loading tweets. Hang on...");
-      $("#message-drawer").show();
+      notify("Loading tweets. Hang on...");
       $(this).children(".loading").show();
       $(this).children("#load-tweets-button").disable();
       
@@ -10,11 +9,11 @@ $(function() {
          data : { "user" : $("input#load-tweets-box").val() },
          success : function(success) {
             $(".loading").hide();
-            $(this).children("#load-tweets-button").enable();
+            $("form#load-tweets-form #load-tweets-button").enable();
             if(success == "true")
-               location.href = "/?status=done";
+               location.href = "/load";
             else
-               $("#message-drawer .message-inside span").html("No such user.");
+               notify("No such user.");
          }
       });
       
@@ -29,21 +28,22 @@ $(function() {
          data: { "remove" : $(this).attr("name") },
          success : function(success) {
             if(success == "true") {
-               $("#message-drawer span").html("User removed.");
+               notify("User removed.");
                location.href = location.href;
             }
             else {
-               $("#message-drawer span").html("No such user in the database.");
+               notify("No such user in the database.");
             }
          }
       });
       return false;
    });
    
+   /* $("form#load-tweets-form input#load-tweets-button").addClass("disabled").attr("disabled", "disabled");
    $("form#load-tweets-form input#load-tweets-box").keyup(check_input);
    $("form#load-tweets-form input#load-tweets-box").click(check_input);
    $("form#load-tweets-form input#load-tweets-box").dblclick(check_input);
-   $("form#load-tweets-form input#load-tweets-box").blur(check_input);
+   $("form#load-tweets-form input#load-tweets-box").blur(check_input); */
    
    $("select#category").change(function() {
       if($(this).val() == "1")
@@ -68,6 +68,11 @@ $(function() {
    
    $("#content, #main-content").css('min-height', ($(window).height()-60)+"px");
 });
+
+function notify(message) {
+   $("#message-drawer").show();
+   $("#message-drawer .message-inside span").html(message);
+}
 
 function check_input() {
    if($(this).val().length == 0) {
